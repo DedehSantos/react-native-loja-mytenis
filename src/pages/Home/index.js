4455938cb6e61b9cc9a65fc4ce860e3aa86cf4ba
Dedeh Santos <dedehfrontend@gmail.com>
2021-12-01 14:04:04 -0300

    subir sem erros

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text,StyleSheet, ScrollView } from 'react-native';
 
 export default function Home() {
  return (
-   <View>
-       <Text style={{marginTop:60, fontSize:25, fontFamily: 'Anton_400Regular'  }}>Pagina de Home</Text>
+      <View>
+       
        </View>
   );
 }
