e1022b13f84b248f409e6c6a40ffe7b46a9db7e5
Dedeh Santos <dedehfrontend@gmail.com>
2021-12-01 15:51:18 -0300

    ok

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,57 @@
 import React from 'react';
-import { View, Text,StyleSheet, ScrollView } from 'react-native';
+import { View, Text,StyleSheet, ScrollView, Image, TouchableOpacity } from 'react-native';
+import {MaterialIcons} from '@expo/vector-icons'
 
 export default function Home() {
  return (
-      <View>
-       
+      <View style={styles.container}>
+          <View style={styles.header}>
+              <Image
+              source={require('../../assets/banner.png')}
+              style={styles.image}
+              />
+
+          <View style={styles.textContainer}>
+            <Text style={styles.text}>TÊNIS</Text>
+            <Text style={styles.text}> - </Text>
+            <Text style={styles.text}>MASCULINO</Text>
+
+            <TouchableOpacity style={{position:'absolute', right: 0, alignSelf: "center" }}>
+              <MaterialIcons
+                name="filter-list"
+                size={24}
+                color="#000"
+              
+              />
+            </TouchableOpacity>
+          </View>   
+
+
+
+           </View>
        </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    width:'100%',
+    backgroundColor: '#fff'
+  },
+  header:{
+    marginBottom:8
+
+  },
+
+  image:{
+    width:'100%'
+  },
+  
+  textContainer:{
+    flexDirection: 'row',
+    marginVertical: '5%',
+    marginHorizontal: '5%'
+  }
+
+})
