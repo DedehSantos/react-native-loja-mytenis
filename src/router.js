04854b2da6b94642e5e9a1366a9f6b19248c19d9
Dedeh Santos <dedehfrontend@gmail.com>
2021-12-01 13:59:42 -0300

    funcionando todos telas

---
+++ src/router.js
@@ -0,0 +1,31 @@
+import React from "react";
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+
+import Home from './pages/Home';
+import Detail from './pages/Detail';
+
+const Stake = createNativeStackNavigator();
+
+function Routes(){
+    return(
+        <NavigationContainer>
+        <Stake.Navigator>
+            <Stake.Screen
+              name="Home"
+              component={Home}
+              options={{headerShown:false}}
+            />
+            <Stake.Screen
+              name="Detail"
+              component={Detail}
+              options={{headerShown:false}}
+            />
+        </Stake.Navigator>
+    </NavigationContainer>
+
+    )
+    }
+
+export default Routes;
